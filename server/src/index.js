619f4b999135b1ff5850a036987e5b96a110d843
Ashan Kaushalya <ashankaushalya952@gmail.com>
2023-05-30 23:25:38 +0530

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import {userRouter} from './routes/users.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/auth',userRouter);
+
+mongoose.connect("mongodb+srv://ashankaushalya97:Ash972023@recipes.xwa6erv.mongodb.net/recipes?retryWrites=true&w=majority")
+
+app.listen(3001,() => console.log('SERVER STARTED!'))
